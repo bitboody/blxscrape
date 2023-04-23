685aaf7232ac715072cb5b7f8b7bf72b8a6fbcff
Boody <bodebi639@gmail.com>
2023-04-23 13:39:00 +0200

    Added dotenv and fixed desc scraping

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
 const fs = require('fs');
+require('dotenv').config({ path:'../.env' });
 
 async function scrape(url, username, password) {
   const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
+
   await page.goto('https://roblox.com/login', { waitUntil: 'networkidle0' });
   await page.type('#login-username', username);
   await page.type('#login-password', password);
@@ -17,7 +20,6 @@ async function scrape(url, username, password) {
 
   await page.goto(url);
 
-
   // username 
   await console.log('Fetching username...');
   const [el] = await page.$x('/html/body/div[3]/main/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div[2]');
@@ -27,7 +29,7 @@ async function scrape(url, username, password) {
   // desc
   await console.log('Fetching description...');
   const [el2] = await page.$x('/html/body/div[3]/main/div[2]/div[2]/div/div[3]/div/div[1]/div[1]/div[2]/div/pre/span');
-  txt = await el2.getProperty('innerText');
+  txt = await page.evaluate(el2 => el2.textContent, el2);
   const descTxt = await el2.jsonValue();
 
 
@@ -37,11 +39,11 @@ async function scrape(url, username, password) {
   const src = await el3.getProperty('src');
   const pfpUrl = await src.jsonValue();
 
-  const data = {usernameTxt, pfpUrl, descTxt};
+  const data = {usernameTxt, txt, pfpUrl};
 
   console.log(data);
 
   browser.close();
 }
 
-scrape('https://www.roblox.com/users/64004875/profile', 'gl0ck11', '12341234islam');
+scrape('https://www.roblox.com/users/64004875/profile', process.env.USER, process.env.PASS);
