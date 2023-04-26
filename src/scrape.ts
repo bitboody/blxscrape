1b0b28357de2e919d1c2b7badf00da1cd17f65d2
Boody <bodebi639@gmail.com>
2023-04-26 17:34:31 +0200

    Saving to JSON file (unfinished)

--- src/scrape.ts
+++ src/scrape.ts
@@ -2,12 +2,22 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const AnonimizeUa = require('puppeteer-extra-plugin-anonymize-ua');
 const path = require('path');
+const fs = require('fs/promises');
 require('dotenv').config({ path: '../config/.env' });
 
 export async function scrape(url?: string, username?: string, password?: string) {
+  const jsonFile = await fs.readFile('../json/data.json');
+  const jsonValue = JSON.parse(jsonFile);
+  
+  if (jsonFile.includes(url) === true) {
+    console.log('Value already exists');
+    return 0;
+  }
+
   const browser = await puppeteer.use(StealthPlugin(), AnonimizeUa())
     .launch({ headless: false, userDataDir: "./user_data" });
   const page = await browser.newPage();
+
   await page.goto('https://www.roblox.com');
 
   if (await page.evaluate(() => document.location.href) !== "https://www.roblox.com/home") {
@@ -26,11 +36,15 @@ export async function scrape(url?: string, username?: string, password?: string)
 
   await page.goto(url);
 
+  // URL
+  console.log('Fetching URL...');
+  let urlTxt: any = await page.evaluate(() => document.location.href);  
+
   // username 
   console.log('Fetching username...');
-  const [el]: any = await page.$x('/html/body/div[3]/main/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div[2]');
-  let txt: any = await el.getProperty('textContent');
-  const usernameTxt: string = await txt.jsonValue();
+  const [el1]: any = await page.$x('/html/body/div[3]/main/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div[2]');
+  let txt: any = await el1.getProperty('textContent');
+  let usernameTxt: JSON = await txt.jsonValue();
 
   // desc
   console.log('Fetching description...');
@@ -46,11 +60,13 @@ export async function scrape(url?: string, username?: string, password?: string)
   console.log('Fetching profile picture...');
   const [el3]: any = await page.$x('/html/body/div[3]/main/div[2]/div[2]/div/div[1]/div/div[2]/div[1]/span/thumbnail-2d/span/img');
   const src: any = await el3.getProperty('src');
-  const pfpUrl: string = await src.jsonValue();
+  const pfpUrl: JSON = await src.jsonValue();
 
-  const data: object = {usernameTxt, descTxt, pfpUrl};
+  const data: object = {urlTxt, usernameTxt, descTxt, pfpUrl};
 
   console.log(data);
+  
+  await fs.writeFile('../json/data.json', `,\n${JSON.stringify(data, null, 4)}`, { flag: 'a' });
 
   browser.close();
 }
