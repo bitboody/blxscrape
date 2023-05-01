5b3b480af9f16d18e9b8aa0e569644ee92bb6d1d
Boody <bodebi639@gmail.com>
2023-05-01 15:31:34 +0300

    Minor syntax change

--- src/scrape.ts
+++ src/scrape.ts
@@ -55,7 +55,7 @@ async function scrape(url: string, user: string, password: string) {
   console.log('Fetching description...');
   const [el2] = await page.$x('/html/body/div[3]/main/div[2]/div[2]/div/div[3]/div/div[1]/div[1]/div[2]/div/pre/span');
   let desc;
-  if (el2 === undefined) {
+  if (!el2) {
     desc = '';
   } else {
     desc = await page.evaluate(el2 => el2.textContent, el2);
